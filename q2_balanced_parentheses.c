b2a174f2e2df1e276182e7a89b154f43bb79a927
m42198317-lab <m42198317@gmail.com>
2026-03-28 19:01:57 +0530

    Add files via upload

---
+++ q2_balanced_parentheses.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char ch) {
+    stack[++top] = ch;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+int main() {
+    char exp[100];
+    printf("Enter expression: ");
+    scanf("%s", exp);
+
+    for(int i = 0; i < strlen(exp); i++) {
+        if(exp[i] == '(')
+            push(exp[i]);
+        else if(exp[i] == ')') {
+            if(top == -1) {
+                printf("Not Balanced");
+                return 0;
+            }
+            pop();
+        }
+    }
+
+    if(top == -1)
+        printf("Balanced Expression");
+    else
+        printf("Not Balanced");
+
+    return 0;
+}
